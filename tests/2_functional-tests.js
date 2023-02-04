4ac3604dba3bd7b011518376f75e7cbc0ade484a
lapanikeugene <eugeny.lp@gmail.com>
2023-02-04 15:14:06 +0100

    test func

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -26,8 +26,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
@@ -36,16 +36,29 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({"surname":"Colombo"})
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status,200);
+          assert.equal(res.type,"application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname,"Colombo");
+
 
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put("/travalers")
+        .send({ "surname": "da Verrazzano"})
+        .end((err,res)=>{
+          assert.equal(res.status,200);
+          assert.equal(res.type,"application/json");
+          assert.equal(res.body.name,"Giovanni");
+          assert.equal(res.body.surname,"da Verrazzano")
+        })
 
       done();
     });
@@ -53,9 +66,14 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://localhost:3000/'; // Your URL here
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
+  const browser = new Browser();
+  suiteSetup((done)=>{
+    return browser.visit("/",done);
+  })
+ this.timeout(5000);
 
 
 
